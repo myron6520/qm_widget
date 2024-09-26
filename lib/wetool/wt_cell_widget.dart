a7b4be29efa4f1f2065ae7b45f757c974ce6aed4
myron6520 <100897740+myron6520@users.noreply.github.com>
2024-09-26 10:38:29 +0800

    Update wt_cell_widget.dart

--- lib/wetool/wt_cell_widget.dart
+++ lib/wetool/wt_cell_widget.dart
@@ -14,30 +14,53 @@ class WTCellWidget extends StatelessWidget {
   final Widget? Function(String title)? titleBuilder;
   final String subTitle;
   final String detail;
+  final Widget? Function(String detail)? detailBuilder;
   final bool showBottomLine;
   final Widget? right;
   final EdgeInsets? padding;
   final EdgeInsets? margin;
   final double? rightSpan;
-  const WTCellWidget({Key? key, required this.title, this.subTitle = "", this.detail = "", this.showBottomLine = true, this.right, this.padding, this.margin, this.rightSpan, this.titleColor = QMColor.COLOR_030319, this.titleBuilder}) : super(key: key);
+  const WTCellWidget({
+    Key? key,
+    required this.title,
+    this.subTitle = "",
+    this.detail = "",
+    this.showBottomLine = true,
+    this.right,
+    this.padding,
+    this.margin,
+    this.rightSpan,
+    this.titleColor = QMColor.COLOR_030319,
+    this.titleBuilder,
+    this.detailBuilder,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) => [
         [
           [
-            titleBuilder?.call(title) ?? title.toText(color: titleColor, fontSize: 16.s, height: 24 / 16),
+            titleBuilder?.call(title) ??
+                title.toText(
+                    color: titleColor, fontSize: 16.s, height: 24 / 16),
             subTitle.isNotEmpty.toWidget(
-              () => subTitle.toText(color: QMColor.COLOR_BDBDBD, fontSize: 14.fs, height: 20 / 14).applyPadding(EdgeInsets.only(top: 4.s)),
+              () => subTitle
+                  .toText(
+                      color: QMColor.COLOR_BDBDBD,
+                      fontSize: 14.fs,
+                      height: 20 / 14)
+                  .applyPadding(EdgeInsets.only(top: 4.s)),
             ),
-          ].toColumn(crossAxisAlignment: CrossAxisAlignment.start, mainAxisAlignment: MainAxisAlignment.center),
+          ].toColumn(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center),
           8.s.inRow,
-          detail
-              .toText(
-                color: QMColor.COLOR_8F92A1,
-                fontSize: 14.fs,
-                height: 20 / 14,
-                textAlign: TextAlign.right,
-              )
+          (detailBuilder?.call(detail) ??
+                  detail.toText(
+                    color: QMColor.COLOR_8F92A1,
+                    fontSize: 14.fs,
+                    height: 20 / 14,
+                    textAlign: TextAlign.right,
+                  ))
               .expanded,
           12.s.inRow,
         ].toRow().applyBackground(
@@ -46,14 +69,19 @@ class WTCellWidget extends StatelessWidget {
             margin: margin ?? EdgeInsets.symmetric(horizontal: 16.s),
             decoration: BoxDecoration(
               color: Colors.white,
-              border: showBottomLine ? Border(bottom: BorderSide(color: QMColor.COLOR_F2F2F2, width: 0.5.s)) : null,
+              border: showBottomLine
+                  ? Border(
+                      bottom:
+                          BorderSide(color: QMColor.COLOR_F2F2F2, width: 0.5.s))
+                  : null,
             )),
         (right ??
                 SvgPicture.string(
                   WTIcon.ARROW_RIGHT_SMALL,
                   width: 24.s,
                   height: 24.s,
-                  colorFilter: ColorFilter.mode(QMColor.COLOR_E0E0E0, BlendMode.srcIn),
+                  colorFilter:
+                      ColorFilter.mode(QMColor.COLOR_E0E0E0, BlendMode.srcIn),
                 ))
             .toPositioned(right: rightSpan ?? 16.s, top: 0, bottom: 0)
       ].toStack();
