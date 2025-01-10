4f5f2cce09794e06a10110d1e0d4de029689d85f
myron6520 <100897740+myron6520@users.noreply.github.com>
2025-01-10 14:28:25 +0800

    Update qm_color.dart

--- lib/style/qm_color.dart
+++ lib/style/qm_color.dart
@@ -35,4 +35,5 @@ class QMColor {
   static const Color COLOR_E8B758 = Color(0xFFE8B758);
   static const Color COLOR_F2F4F5 = Color(0xFFF2F4F5);
   static const Color COLOR_8B8D8C = Color(0xFF8B8D8C);
+  static const Color COLOR_D9D9D9 = Color(0xFFD9D9D9);
 }
